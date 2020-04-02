2ebe9c0e1b6e25662aece922d1288d7efca0d586
Erik Raith <raithhtiar@gmail.com>
2020-04-02 11:17:46 -0600

    [feat] Add Form testing & skeleton App Integration

--- src/App/App.test.js
+++ src/App/App.test.js
@@ -1,9 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import App from './App';
+import "@testing-library/jest-dom";
+import { render, fireEvent, waitFor } from "@testing-library/react";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+it('renders without crashing', async () => {
+  const {getByPlaceholderText, getByLabelText, getByText,debug } = render(
+    <App/>
+  );
+
+  const leta = await waitFor(() => getByText("Leta"))
+  expect(leta).toBeInTheDocument()
+
+  debug()
+
 });
