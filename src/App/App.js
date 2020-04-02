a34feed26ec6be97c23dfa48919986bebfbd947a
Erik Raith <raithhtiar@gmail.com>
2020-04-02 10:42:16 -0600

    [feat] Add controlled form make Reservation button to change state

--- src/App/App.js
+++ src/App/App.js
@@ -1,37 +1,42 @@
-import React, { Component } from 'react';
-import './App.css';
-import Reservations from '../Reservations/Reservations'
-import Form from '../Form/Form'
+import React, { Component } from "react";
+import "./App.css";
+import Reservations from "../Reservations/Reservations";
+import Form from "../Form/Form";
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      reservations:[]
+      reservations: []
     };
   }
 
   componentDidMount() {
-    fetch('http://localhost:3001/api/v1/reservations')
-    .then(res => res.json())
-    .then(reservations => this.setState({reservations}))
-    .catch(err => console.error(err.message))
+    fetch("http://localhost:3001/api/v1/reservations")
+      .then(res => res.json())
+      .then(reservations => this.setState({ reservations }))
+      .catch(err => console.error(err.message));
   }
+
+
+  makeNewReservation = newReservationData => {
+    this.setState({
+      reservations: [...this.state.reservations, newReservationData]
+    });
+  };
 
   render() {
     return (
       <div className="App">
-        <h1 className='app-title'>Turing Cafe Reservations</h1>
-        <div className='resy-form'>
-          <Form/>
+        <h1 className="app-title">Turing Cafe Reservations</h1>
+        <div className="resy-form">
+          <Form makeNewReservation={this.makeNewReservation} />
         </div>
-        <div className='resy-container'>
-          <Reservations
-            reservations = {this.state.reservations}
-          />
+        <div className="resy-container">
+          <Reservations reservations={this.state.reservations} />
         </div>
       </div>
-    )
+    );
   }
 }
 
