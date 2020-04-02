826ee9260c593d4e4b5b5c4532220a23e95f05fd
Erik Raith <raithhtiar@gmail.com>
2020-04-02 09:53:45 -0600

    [feat] Add map to reservations to create the cards

--- src/Reservations/Reservations.js
+++ src/Reservations/Reservations.js
@@ -1,12 +1,24 @@
-import React from 'react';
+import React from "react";
+import Card from "../Card/Card";
 
+const Reservations = ({ reservations }) => {
+  const reservationCards = reservations.map(reservation => {
+    return <Card
+      id={reservation.id}
+      date={reservation.date}
+      name={reservation.name}
+      number={reservation.number}
+      time={reservation.time}
+      key = {reservation.id}
+    />;
+  });
 
-const Reservations = ({reservations}) => {
   return (
     <>
       <h2> Reservations go here!!!</h2>
+      {reservationCards}
     </>
-  )
-}
+  );
+};
 
 export default Reservations;
