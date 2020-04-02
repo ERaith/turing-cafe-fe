d9a8a075691b4dcc51d950685e39d4ab44dae8d1
Erik Raith <raithhtiar@gmail.com>
2020-04-02 09:56:54 -0600

    [test] Update test

--- src/Reservations/Reservations.test.js
+++ src/Reservations/Reservations.test.js
@@ -3,8 +3,16 @@ import Resrvations from './Resrvations';
 import { render, fireEvent } from '@testing-library/react';
 import '@testing-library/jest-dom';
 
+const mockReservationData;
 
 beforeEach(() => {
+  mockReservationData = {
+    id=1,
+    date='4/5',
+    name='Leta',
+    number=12,
+    time="7:00"
+  }
 
 });
 
@@ -12,11 +20,7 @@ it('should render when given the right props', () => {
   // we need to mock removeIdea
     const {getByText} = render(
       <Reservations
-      id={1}
-      date={'4/5'}
-      name={'Leta'}
-      number={12}
-      time={"7:00"}
+        reservations = {mockReservationData}
       />
     )
 
