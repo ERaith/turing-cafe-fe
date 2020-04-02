64bba00342cd9654229bd2b3261bb8e16440985b
Erik Raith <raithhtiar@gmail.com>
2020-04-02 09:34:11 -0600

    [feat][wip] Add Reservations function and recieve props

---
+++ src/Reservations/Reservations.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const Reservations = ({reservations}) => {
+  return (
+    <>
+      <h2> Reservations go here!!!</h2>
+    </>
+  )
+}
+
+export default Reservations;
